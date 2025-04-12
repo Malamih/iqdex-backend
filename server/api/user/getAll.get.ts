85c89f8bffed243155f713178bb65c3fa15b3300
amer-wild11 <wild4bs@gmail.com>
2025-04-12 21:06:00 +0300

    updates

--- server/api/user/getAll.get.ts
+++ server/api/user/getAll.get.ts
@@ -1,22 +1,7 @@
 import prisma from "~/lib/prisma";
-import { writeFile } from "fs/promises";
 
+// استخدم التقسيم إلى صفحات مع دفق البيانات
 export default defineEventHandler(async (event) => {
-  const batchSize = 1000; // عدد المستخدمين في كل دفعة
-  try {
-    const users = await prisma.user.findMany();
-
-    return {
-      ok: true,
-      users,
-    };
-  } catch (error: any) {
-    sendError(
-      event,
-      createError({
-        statusCode: 500,
-        statusMessage: error.message,
-      })
-    );
-  }
+  const users = await prisma.user.findMany({});
+  return { users };
 });
